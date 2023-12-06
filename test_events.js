aed0d8d25553e465b1d3a5810aae407204c669f0
Yar <122922753+earlievanti@users.noreply.github.com>
2023-12-06 17:25:44 +0300

    Create test_events.js

---
+++ test_events.js
@@ -0,0 +1,13 @@
+// Example of working with events:
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Event Handling</title>
+</head>
+<body>
+     <button onclick="alert('Button Clicked!')">Click Me</button>
+</body>
+</html>
